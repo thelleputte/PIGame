cf1afc0ed2233b6a8bb0898c72b2a65ce1b6bec0
Ben <ben.caby@gmail.com>
2016-12-23 00:25:19 +0100

    debouncing OK finally

--- ben/pigState.py
+++ ben/pigState.py
@@ -50,7 +50,7 @@ class PigState():
 	def ack_button_configure_gpio(self, gpio):
 		self.set_gpio_direction(gpio, "in")
 		self.set_gpio_active_low(gpio,"1") #if pulled up an active low comportment is more logic : button pressed = 1
-		self.set_gpio_trigger(gpio,"both")
+		self.set_gpio_trigger(gpio,"falling")
 
 	def __str__(self):
 		return self.name
@@ -135,22 +135,45 @@ class WaitForAnswerState(PigState):
 			epoll.register(gpios[p.id]["fd"].fileno(), select.EPOLLPRI) #demons
 			gpios[p.id]["fd"].read()
 			#gpios[p.id]["fd"].seek(0,0)
+		nack = open(self.game.ack_buttons["nack_button"]["path"] +"/value",'r')
+		epoll.register(nack.fileno(), select.EPOLLPRI)
+		nack.read()
+		
 		events = epoll.poll(-1)
 		for fileno, event in events:
+			print("longueur de events = {}".format(len(events)))
+			if fileno == nack.fileno():
+				nack.read()
+				player = None
+				self.game.answer_from(player)
+				print("Next Question Please !!")
+				self.game.set_state(self.game.wait_for_answer_state)
+				#debouncing :
+				debounce_buffer = ['1\n']*5
+				while '1\n' in debounce_buffer:
+					nack.seek(0,0)
+					debounce_buffer.append(nack.read())
+					debounce_buffer.pop(0)
+					time.sleep(0.01)
+				break
 			player = [p for p in self.game.players if gpios[p.id]["fd"].fileno() == fileno]
 			#player = self.game.players(gpios["buttons"].index(button))
 			self.game.answer_from(player[0])
 			#print("debug pigState line : {}, fastest player = {}".format(139, player))
+		
 		for p in self.game.players:
 			epoll.unregister(gpios[p.id]["fd"])
 			gpios[p.id]["fd"].close()
+		epoll.unregister(nack.fileno())
+		nack.close()
 		epoll.close()
 		
-		#now that the player is known, we need to light his button
-		light = open(str(gpios[player[0].id]["light"])+"/value",'w')
-		light.write("1")
-		light.close()
-		self.game.set_state(self.game.handle_answer_state)
+		if player is not None:
+			#now that the player is known, we need to light his button
+			light = open(str(gpios[player[0].id]["light"])+"/value",'w')
+			light.write("1")
+			light.close()
+			self.game.set_state(self.game.handle_answer_state)
 
 class HandleAnswerState(PigState):
 	def __init__(self, game):
@@ -175,54 +198,72 @@ class WaitForAnswerAckState(PigState):
 		epoll = select.epoll()
 		for i in self.ack_buttons:
 			fd.append((open(self.ack_buttons[i]["path"]+"/value",'r'),i))
+			fd[-1][0].read()
 			epoll.register(fd[-1][0].fileno(),select.EPOLLPRI)
 			fd[-1][0].read()
+			
+			
 		#wait for an action on one of the 2 buttons
 		pressed_button = list()
 		events = epoll.poll(-1)
+		print("length of events in wait_ack {}".format(len(events)))
 		for fileno, event in events:
 			pressed_button.append(fileno)
-			first_press = time.perf_counter()
+			#first_press = time.perf_counter()
 			print("first press = {}".format(str(first_press)))
 			for f in [x for x in fd if x[0].fileno() == fileno] :
 				f[0].read()
 				f[0].seek(0,0)
-				active_file = f[0]
+				active_file = f
+				#debouncing :
+				debounce_buffer = ['1\n']*5
+				while '1\n' in debounce_buffer:
+					f[0].seek(0,0)
+					debounce_buffer.append(f[0].read())
+					debounce_buffer.pop(0)
+					#print(debounce_buffer)
+					time.sleep(0.01)
 				
 		#keep only the pressed button in epoll list
 		rem = [f for f in fd if f[0].fileno() != pressed_button[0]]
 		for f in rem:
 			epoll.unregister(f[0].fileno())
-		time.sleep(0.1)#debouncing
-		active_file.seek(0,0)
-		active_file.read()#have to read after registering/unregistering
-		events = epoll.poll(self.next_question_timeout)
-		second_press = time.perf_counter()-first_press
-		print("second press = {}".format(str(second_press)))
+		epoll.unregister(active_file[0].fileno())
+		# time.sleep(0.1)#debouncing
+		# active_file.seek(0,0)
+		# active_file.read()#have to read after registering/unregistering
+		# events = epoll.poll(self.next_question_timeout)
+		# second_press = time.perf_counter()-first_press
+		# print("second press = {}".format(str(second_press)))
 		epoll.close()
-		if len(events)<1:
-			#the button has been pressed for more than next_qestion_timeout
-			#that means next question please
-			return_value = None #as there is no winner for this question
+		# if len(events)<1:
+			# the button has been pressed for more than next_qestion_timeout
+			# that means next question please
+			# return_value = None #as there is no winner for this question
+		# else :
+			# for fileno, event in events:
+				# ack= [a[1] for a in fd if a[0].fileno() == fileno]
+				# print(ack[0])
+				# if ack[0] == "ack_button":
+					# return_value = True
+				# elif ack[0] == "nack_button":
+					# return_value = False
+				# else:
+					# return_value = None #demons
+		
+		if active_file[1] == "ack_button":
+			return_value = True
 		else :
-			for fileno, event in events:
-				ack= [a[1] for a in fd if a[0].fileno() == fileno]
-				print(ack[0])
-				if ack[0] == "ack_button":
-					return_value = True
-				elif ack[0] == "nack_button":
-					return_value = False
-				else:
-					return_value = None #demons
+			return_value = False
 		for f in fd:
-			#print(f[0])
 			f[0].close()
 		return return_value
 		
 	def handle_state(self):
 		val = self.wait_ack()
-		f = open("/sys/class/gpio/gpio{}/value".format(self.game.fastest_player.gpio[1]),'w')
-		f.write("0")
+		if self.game.fastest_player:
+			f = open("/sys/class/gpio/gpio{}/value".format(self.game.fastest_player.gpio[1]),'w')
+			f.write("0")
 		if val is True :
 			print("good answer, give the point ")
 			#give the point
@@ -240,7 +281,7 @@ class WaitForAnswerAckState(PigState):
 			self.game.set_state(self.game.wait_for_answer_state)
 			#demons should remove the fastest player from player list for next wait for answer
 		else: #should be None => next question
-			print("next question please")
+			print("next question please line 264")
 			#reset the fastest player
 			self.game.answer_from(None)
 			#demons : we should reset the list of player as we go to next question
